f243afb07d1a8eec4aeca3fb9cafe8c1b50d1da0
kim yongbin <kybinz@gmail.com>
2017-07-02 18:13:23 +0900

    write basic server.

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+	err := http.ListenAndServe(":8080", nil)
+	log.Fatal(err)
+}
