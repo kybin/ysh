90d1b4e52bbcaa9757fef3c0fb257806e62d858b
kim yongbin <kybinz@gmail.com>
2017-07-02 18:25:30 +0900

    use template to show shell dir in index.html.

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type shell struct {
+	Dir string
+}
+
+var shellTmpl = template.Must(template.ParseFiles("index.html"))
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "index.html")
+		sh := shell{Dir: "/home/kybin"}
+		err := shellTmpl.Execute(w, sh)
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 	err := http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
