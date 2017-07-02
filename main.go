8c94225f80c2d10ef8e2af916cafef5c288bc017
kim yongbin <kybinz@gmail.com>
2017-07-02 20:48:29 +0900

    implement basic web socket using golilla/websocket.

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
 
 type shell struct {
@@ -12,12 +15,38 @@ type shell struct {
 
 var shellTmpl = template.Must(template.ParseFiles("index.html"))
 
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		sh := shell{Dir: "/home/kybin"}
 		err := shellTmpl.Execute(w, sh)
 		if err != nil {
 			log.Fatal(err)
+		}
+	})
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		conn, err := upgrader.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for {
+			_, p, err := conn.ReadMessage()
+			if err != nil {
+				// if session is closed, it will raise 'going away' error.
+				// so this function will return.
+				log.Print(err)
+				return
+			}
+			fmt.Println(string(p))
+			err = conn.WriteMessage(websocket.TextMessage, []byte("hello"))
+			if err != nil {
+				log.Print(err)
+				return
+			}
 		}
 	})
 	err := http.ListenAndServe(":8080", nil)
