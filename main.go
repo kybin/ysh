f6558b242589865e93c5a5a712a13957fc332301
kim yongbin <kybinz@gmail.com>
2017-07-06 23:23:31 +0900

    make current directory changeable.

--- main.go
+++ main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
 
 type shell struct {
+	mu  sync.Mutex
 	Dir string
+}
+
+func (s *shell) changeDir(d string) error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	info, err := os.Stat(d)
+	if err != nil {
+		return err
+	}
+	if !info.IsDir() {
+		return fmt.Errorf("%v is not a directory", d)
+	}
+	s.Dir = d
+	fmt.Println(s)
+	return nil
+}
+
+func (s *shell) runCommand(cmd string) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if cmd == "" {
+		return []byte{}, nil
+	}
+	cmds := strings.Split(strings.TrimSpace(cmd), " ")
+
+	name := cmds[0]
+	args := cmds[1:]
+
+	c := exec.Command(name, args...)
+	c.Dir = s.Dir
+	return c.CombinedOutput()
 }
 
 var shellTmpl = template.Must(template.ParseFiles("index.html"))
@@ -23,12 +60,12 @@ var upgrader = websocket.Upgrader{
 }
 
 func main() {
+	dir, err := filepath.Abs(".")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sh := shell{Dir: dir}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		dir, err := filepath.Abs(".")
-		if err != nil {
-			log.Fatal(err)
-		}
-		sh := shell{Dir: dir}
 		err = shellTmpl.Execute(w, sh)
 		if err != nil {
 			log.Fatal(err)
@@ -47,23 +84,33 @@ func main() {
 				log.Print(err)
 				return
 			}
-
-			cmdstr := string(p)
-			if cmdstr == "" {
-				continue
+			msg := strings.Split(string(p), ":")
+			if len(msg) != 2 {
+				log.Printf("invalid message: %v", msg)
+				return
 			}
-			cmds := strings.Split(strings.TrimSpace(cmdstr), " ")
-
-			cmd := cmds[0]
-			args := cmds[1:]
+			typ := msg[0]
+			body := msg[1]
 
 			result := []byte{}
-			c := exec.Command(cmd, args...)
-			out, err := c.CombinedOutput()
-			if err != nil {
-				result = []byte("error: " + err.Error())
-			} else {
-				result = out
+			switch typ {
+			case "cmd":
+				out, err := sh.runCommand(body)
+				if err != nil {
+					result = []byte("error:" + err.Error())
+				} else {
+					result = append([]byte("ok:"), out...)
+				}
+			case "dir":
+				err := sh.changeDir(body)
+				if err != nil {
+					result = []byte("error:" + err.Error())
+				} else {
+					result = []byte("ok:")
+				}
+			default:
+				log.Printf("unknown message type: %v", typ)
+				return
 			}
 
 			err = conn.WriteMessage(websocket.TextMessage, result)
@@ -73,6 +120,6 @@ func main() {
 			}
 		}
 	})
-	err := http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":8080", nil)
 	log.Fatal(err)
 }
